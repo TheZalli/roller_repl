6a9d2bb070099da25fc33b64fd30076290dc073a
mrZalli <misterzalli@gmail.com>
2017-09-03 01:20:53 +0300

    Added lalrpop parsing from lalrpop tutorial

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,62 @@
 extern crate libc;
 extern crate rustyline;
 
+mod parser;
+
 use rustyline::error::ReadlineError;
+use parser::expr;
 
 fn main() {
+    ::std::process::exit(real_main());
+}
+
+fn real_main() -> i32 {
+    // return value
+    let mut return_status = 1;
+
     let is_in_tty = unsafe { libc::isatty(libc::STDIN_FILENO as i32) } != 0;
-    let is_out_tty = unsafe { libc::isatty(libc::STDOUT_FILENO as i32) } != 0;
+    //let is_out_tty = unsafe { libc::isatty(libc::STDOUT_FILENO as i32) } != 0;
 
-    let mut rl = rustyline::Editor::<()>::new().history_ignore_space(true);
-    let prompt = "> "; //if is_in_tty { "> " } else { "" };
+    let mut rl = rustyline::Editor::<()>::new();
+    let prompt = "> ";
 
-    if is_in_tty && is_out_tty {
-        println!("Interactive REPL started");
+    if is_in_tty {
+        println!("Interactive Roller REPL started");
     }
 
     loop {
         // read a line
         let line = rl.readline(prompt);
         match line {
-            Ok(ref s) if s.trim() == "" => continue, // empty input
-            Ok(s) => println!("Hello, {}!", s),
+            Ok(ref input) if input.trim() == "" => continue, // empty input
+
+            Ok(input) => {
+                // ok input
+                rl.add_history_entry(&input.trim_right());
+                let input = input.trim();
+                let parsed_res = expr::parse_Expr(input);
+
+                println!("Result is: {:?}", parsed_res);
+            },
+
             // TODO: maybe check ReadlineError::WindowResize when on windows
+
             Err(ReadlineError::Interrupted) => {
-                // received interrupt (ctrl + C)
-                println!("Interrupt received, exiting");
+                // received interrupt (ctrl+C)
+                println!("Interrupt received");
+                return_status = 0;
                 break;
             },
+
             Err(ReadlineError::Eof) => {
-                // received EOF (ctrl + D, or end of pipe input)
-                println!("End-of-line received, exiting");
+                // received EOF (ctrl+D, or end of pipe input)
+                if is_in_tty {
+                    println!("End-of-file received");
+                }
+                return_status = 0;
                 break;
             },
+
             Err(e) => {
                 // other error, maybe IO error
                 // in some cases we might want to continue, but we don't want 
@@ -40,4 +66,7 @@ fn main() {
             },
         }
     }
+    println!("Exiting");
+
+    return_status
 }
